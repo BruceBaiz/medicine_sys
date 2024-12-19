f8a06b08c4db632be093891e48ee56da4fbe52c7
Bruce <1971226446@qq.com>
2024-12-19 08:42:44 +0800

    初始提交

--- src/main/java/com/example/mapper/DeviceMapper.java
+++ src/main/java/com/example/mapper/DeviceMapper.java
@@ -1,11 +1,23 @@
 package com.example.mapper;
 
+import com.example.entity.Device;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface DeviceMapper {
 
     // 查询所有设备
-    List<>
+    public List<Device> selectAllDevice();
+
+    // 根据设备id查询设备
+    public Device selectDeviceById(Integer id);
+
+    // 添加设备
+    public int addDevice(Device device);
+
+    //
+
 
 }
